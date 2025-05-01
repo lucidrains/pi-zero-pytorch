51dac5df5a0ff95bced359b9384426d3979c5725
lucidrains <lucidrains@gmail.com>
2025-05-01 17:22:20 +0000

    add generalized advantage estimate, and make sure can derive actor and critic loss end to end

--- tests/test_pi_zero.py
+++ tests/test_pi_zero.py
@@ -2,6 +2,7 @@ import pytest
 
 import torch
 from pi_zero_pytorch import π0
+from einops import repeat, rearrange
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
@@ -59,10 +60,14 @@ def test_pi_zero_with_vit(
 
     assert sampled_actions.shape == (2, 32, 6)
 
-def optimize_policy():
-    from pi_zero_pytorch import (
+def test_policy_optimization():
+    from vit_pytorch import ViT
+    from vit_pytorch.extractor import Extractor
+
+    from pi_zero_pytorch.pi_zero import (
         Agent,
         EPO,
+        calc_generalized_advantage_estimate
     )
 
     v = ViT(
@@ -90,11 +95,11 @@ def optimize_policy():
         policy_optimizable = True
     ).to(device)
 
-    images = torch.randn(2, 3, 2, 256, 256).to(device)
-    commands = torch.randint(0, 20_000, (2, 1024)).to(device)
+    images = torch.randn(1, 3, 2, 256, 256).to(device)
+    commands = torch.randint(0, 20_000, (1, 1024)).to(device)
 
-    joint_state = torch.randn(2, 12).to(device)
-    actions = torch.randn(2, 32, 6).to(device)
+    joint_state = torch.randn(1, 12).to(device)
+    actions = torch.randn(1, 32, 6).to(device)
 
     loss, _ = model(images, commands, joint_state, actions)
     loss.backward()
@@ -103,23 +108,61 @@ def optimize_policy():
 
     agent = Agent(model)
 
-    final_action_to_env, (
+    steps = 4
+
+    all_input_tensors = []
+    all_replay_tensors = []
+
+    for _ in range(steps):
+        input_tensors = [images, commands, joint_state]
+
+        final_action_to_env, replay_tensors = agent.actor(
+            *input_tensors,
+            trajectory_length = 32,
+            steps = 2,
+            return_states_for_replay = True
+        )
+
+        all_input_tensors.append(input_tensors)
+        all_replay_tensors.append(replay_tensors)
+
+    (
         actions,
         timesteps,
         sampled_flows,
         log_probs
-    ) = agent.actor(
+    ) = map(torch.cat, zip(*all_replay_tensors))
+
+    (
         images,
         commands,
         joint_state,
-        trajectory_length = 32,
-        steps = 4,
-        return_states_for_replay = True
+    ) = map(torch.cat, zip(*all_input_tensors))
+
+
+    t = actions.shape[1]
+
+    values, _ = agent.critic(
+        repeat(images, 'b ... -> (b t) ...', t = t),
+        repeat(commands, 'b ... -> (b t) ...', t = t),
+        repeat(joint_state, 'b ... -> (b t) ...', t = t),
+        actions = rearrange(actions, 'b t ... -> (b t) ...'),
+        times = rearrange(timesteps, 'b t ... -> (b t) ...')
     )
+
+    values = rearrange(values, '(b t) -> t b', t = t)
 
     # actions go out into the environment, rewards are received, generalized advantage calculated with critic values
 
-    advantages = torch.randn(2).to(device)
+    rewards = torch.randn(steps).to(device)
+    boundaries = torch.randint(0, 2, (steps,)).to(device)
+
+    rewards = repeat(rewards, 'b -> t b', t = t)
+    boundaries = repeat(boundaries, 'b -> t b', t = t)
+
+    advantages = calc_generalized_advantage_estimate(rewards, values.detach(), boundaries, use_accelerated = False)
+
+    advantages = rearrange(advantages, 't b -> b t')
 
     # optimize policy with replay tensors from above
 
@@ -135,3 +178,14 @@ def optimize_policy():
     )
 
     actor_loss.backward()
+
+    critic_loss = agent.critic.forward_for_critic_loss(
+        repeat(images, 'b ... -> (t b) ...', t = t),
+        repeat(commands, 'b ... -> (t b) ...', t = t),
+        repeat(joint_state, 'b ... -> (t b) ...', t= t),
+        rearrange(actions, 'b t ... -> (t b) ...'),
+        old_values = values,
+        advantages = advantages,
+    )
+
+    critic_loss.backward()
