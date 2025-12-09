78ac378e155a4faa9ee62a209d1b8b10b5f17b7c
lucidrains <lucidrains@gmail.com>
2025-12-09 07:15:28 -0800

    support train time RTC

--- tests/test_pi_zero.py
+++ tests/test_pi_zero.py
@@ -355,3 +355,29 @@ def test_pi_zero_six():
         loss.backward()
 
     # repeat
+
+def test_train_time_rtc():
+    from pi_zero_pytorch import π0
+
+    model = π0(
+        dim = 512,
+        dim_action_input = 6,
+        dim_joint_state = 12,
+        num_tokens = 20_000,
+        train_time_rtc = True,
+        train_time_rtc_max_delay = 4
+    )
+
+    vision = torch.randn(1, 1024, 512)
+    commands = torch.randint(0, 20_000, (1, 1024))
+    joint_state = torch.randn(1, 12)
+    actions = torch.randn(1, 32, 6)
+
+    loss, _ = model(vision, commands, joint_state, actions)
+    loss.backward()
+
+    # after much training
+
+    sampled_actions = model(vision, commands, joint_state, frozen_actions = actions[:, -3:], trajectory_length = 32) # (1, 32, 6)
+
+    assert sampled_actions.shape == (1, 32 - 3, 6)
